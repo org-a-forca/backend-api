4e029cf9a554f8ddb8b9b516b43bc3f64188dbcb
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-17 22:06:44 -0300

    Adiciona mensagem quando não houver nome da categoria

--- src/main/java/br/com/aforca/aforcaadminapi/dtos/CategoriaDto.java
+++ src/main/java/br/com/aforca/aforcaadminapi/dtos/CategoriaDto.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.Size;
 public class CategoriaDto {
   private Long id;
 
-  @NotEmpty
+  @NotEmpty(message = "O nome da categoria é obrigatório")
   @Size(max = 50, message = "O nome da categoria deve ter no máximo 50 caracteres")
   private String nome;
 }
