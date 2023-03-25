5e1e15c0b2b520aef38bff97e0a867ac4b9c9d25
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-25 19:46:08 -0300

    Adiciona busca pelo nome da categoria no repository

--- src/main/java/br/com/aforca/admin/domain/repository/CategoriaRepository.java
+++ src/main/java/br/com/aforca/admin/domain/repository/CategoriaRepository.java
@@ -3,6 +3,9 @@ package br.com.aforca.admin.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.aforca.admin.domain.entity.Categoria;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+  @Query(value = "select * from categoria where lower(nome) = lower(:nome)", nativeQuery = true)
+  Categoria findByNome(String nome);
 }
