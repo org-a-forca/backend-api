53ae06450853f44aa396fce16e8e3b8e6f9cd2e9
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-25 18:40:19 -0300

    Refatora campo nome da categoria para não ser vazio

--- src/main/java/br/com/aforca/admin/api/model/CategoriaDto.java
+++ src/main/java/br/com/aforca/admin/api/model/CategoriaDto.java
@@ -2,14 +2,14 @@ package br.com.aforca.admin.api.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 public class CategoriaDto {
   private Long id;
 
-  @NotEmpty(message = "O nome da categoria é obrigatório")
+  @NotBlank(message = "O nome da categoria é obrigatório")
   @Size(max = 50, message = "O nome da categoria deve ter no máximo 50 caracteres")
   private String nome;
 }
