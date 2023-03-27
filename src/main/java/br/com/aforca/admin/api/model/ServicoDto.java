d75a33a0baf93fe9cdae88063097245c1c8d45a3
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-27 18:26:06 -0300

    Refatora o requisito do nome do serviço para NotBlank

--- src/main/java/br/com/aforca/admin/api/model/ServicoDto.java
+++ src/main/java/br/com/aforca/admin/api/model/ServicoDto.java
@@ -4,7 +4,7 @@ import br.com.aforca.admin.api.annotation.ServicoNomeUnico;
 import br.com.aforca.admin.domain.entity.Categoria;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 public class ServicoDto {
   private Long id;
 
-  @NotEmpty(message = "O nome do serviço é obrigatório")
+  @NotBlank(message = "O nome do serviço é obrigatório")
   @Size(max = 75, message = "O nome do serviço deve ter no máximo 75 caracteres")
   @ServicoNomeUnico
   private String nome;
