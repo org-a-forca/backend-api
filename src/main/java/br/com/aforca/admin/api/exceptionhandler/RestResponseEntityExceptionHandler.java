8a4dd420697cc1afe4c50162173ba688ef0d3bca
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-27 17:01:56 -0300

    Implementa tratativa ao tentar excluir registro que está sendo utilizado por outro

--- src/main/java/br/com/aforca/admin/api/exceptionhandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/aforca/admin/api/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.aforca.admin.api.exceptionhandler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +36,14 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     bodyOfResponse.put("mensagem", "Registro não encontrado");
 
     return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<Object> handleDataIntegrityViolationException() {
+    Map<String, Object> bodyOfResponse = new HashMap<>();
+    bodyOfResponse.put("mensagem", "Não é possível excluir o registro porque está sendo utilizado por outro");
+
+    return new ResponseEntity<>(bodyOfResponse, HttpStatus.CONFLICT);
   }
 
   @ExceptionHandler(value = { RuntimeException.class })
