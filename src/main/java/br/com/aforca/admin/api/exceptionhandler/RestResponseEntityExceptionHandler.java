bb50886cbe617217f7e9561aed1e6bcf1b5e1b17
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-27 16:18:33 -0300

    Refatora classe que trata exceções na API

--- src/main/java/br/com/aforca/admin/api/exceptionhandler/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/aforca/admin/api/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -6,14 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
     Map<String, String> errors = new HashMap<>();
@@ -24,5 +27,21 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
     });
 
     return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = { NoSuchElementException.class })
+  protected ResponseEntity<Object> handleNoSuchElementException(RuntimeException ex, WebRequest request) {
+    Map<String, Object> bodyOfResponse = new HashMap<>();
+    bodyOfResponse.put("mensagem", "Registro não encontrado");
+
+    return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+  @ExceptionHandler(value = { RuntimeException.class })
+  protected ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+    Map<String, Object> bodyOfResponse = new HashMap<>();
+    bodyOfResponse.put("mensagem", "Erro interno. Contatar o desenvolvedor");
+
+    return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
   }
 }
