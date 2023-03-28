d298c4c1134e364a00059c439937ef187b91cc2b
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-27 21:12:52 -0300

    Refatora DTO da entidade Servico

--- src/main/java/br/com/aforca/admin/api/model/ServicoDto.java
+++ src/main/java/br/com/aforca/admin/api/model/ServicoDto.java
@@ -1,22 +1,10 @@
 package br.com.aforca.admin.api.model;
 
-import br.com.aforca.admin.api.annotation.ServicoNomeUnico;
-import br.com.aforca.admin.domain.entity.Categoria;
 import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Data
 public class ServicoDto {
   private Long id;
-
-  @NotBlank(message = "O nome do serviço é obrigatório")
-  @Size(max = 75, message = "O nome do serviço deve ter no máximo 75 caracteres")
-  @ServicoNomeUnico
   private String nome;
-
-  @NotNull(message = "A categoria do serviço é obrigatória")
-  private Categoria categoria;
+  private CategoriaDto categoria;
 }
