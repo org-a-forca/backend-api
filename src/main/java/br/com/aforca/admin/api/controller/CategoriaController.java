9a6d0c1f58d3841946fbe05fba1469983aeb803f
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-03-29 17:20:45 -0300

    Refatora método DELETE de categoria

--- src/main/java/br/com/aforca/admin/api/controller/CategoriaController.java
+++ src/main/java/br/com/aforca/admin/api/controller/CategoriaController.java
@@ -5,10 +5,12 @@ import br.com.aforca.admin.api.model.CategoriaDto;
 import br.com.aforca.admin.api.model.NovaCategoriaDto;
 import br.com.aforca.admin.domain.service.CategoriaService;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -51,8 +53,16 @@ public class CategoriaController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable @NotNull @Positive Long id) {
-        categoriaService.detele(id);
+    public ResponseEntity<Object> delete(@PathVariable @NotNull @Positive Long id) {
+        try {
+            categoriaService.detele(id);
+
+            return ResponseEntity.noContent().build();
+        } catch (DataIntegrityViolationException ex) {
+            Map<String, Object> corpoDaResposta = new HashMap<>();
+            corpoDaResposta.put("mensagem", "Essa categoria está sendo utilizada em outros registros");
+
+            return new ResponseEntity<>(corpoDaResposta, HttpStatus.CONFLICT);
+        }
     }
 }
