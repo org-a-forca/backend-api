e3a1a9453ea3648e99073a72fe65c99cb0dd2352
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-04-03 20:55:10 -0300

    Remove quebra de linha entre atributos

--- src/main/java/br/com/aforca/admin/api/model/ServicoResumoDto.java
+++ src/main/java/br/com/aforca/admin/api/model/ServicoResumoDto.java
@@ -1,14 +1,10 @@
 package br.com.aforca.admin.api.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ServicoResumoDto {
   private Long id;
-
   private String nome;
-
-  @JsonProperty("categoria_nome")
   private String categoriaNome;
 }
