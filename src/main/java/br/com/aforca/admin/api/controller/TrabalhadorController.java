fba2845e92f48fddb379cd9bec34785e6f1ab07b
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-04-13 01:48:30 -0300

    Implementa cadastro de trabalhador

--- src/main/java/br/com/aforca/admin/api/controller/TrabalhadorController.java
+++ src/main/java/br/com/aforca/admin/api/controller/TrabalhadorController.java
@@ -1,13 +1,26 @@
 package br.com.aforca.admin.api.controller;
 
+import br.com.aforca.admin.api.model.NovoTrabalhadorDto;
 import br.com.aforca.admin.domain.service.TrabalhadorService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/trabalhador")
 @AllArgsConstructor
 public class TrabalhadorController {
   private final TrabalhadorService trabalhadorService;
+
+  @PostMapping
+  public ResponseEntity<Object> create(@RequestBody @Valid @NotNull NovoTrabalhadorDto novoTrabalhadorDto) {
+    return new ResponseEntity<>(trabalhadorService.create(novoTrabalhadorDto), HttpStatus.CREATED);
+  }
 }
--- src/main/java/br/com/aforca/admin/domain/service/TrabalhadorService.java
+++ src/main/java/br/com/aforca/admin/domain/service/TrabalhadorService.java
@@ -1,13 +1,39 @@
 package br.com.aforca.admin.domain.service;
 
 import br.com.aforca.admin.api.mapper.TrabalhadorMapper;
+import br.com.aforca.admin.api.model.NovoTrabalhadorDto;
+import br.com.aforca.admin.api.model.TrabalhadorDto;
+import br.com.aforca.admin.domain.entity.Servico;
+import br.com.aforca.admin.domain.repository.ServicoRepository;
 import br.com.aforca.admin.domain.repository.TrabalhadorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TrabalhadorService {
   private final TrabalhadorRepository trabalhadorRepository;
   private final TrabalhadorMapper trabalhadorMapper;
+  private final ServicoRepository servicoRepository;
+
+  public TrabalhadorDto create(@Valid @NotNull NovoTrabalhadorDto novoTrabalhadorDto) {
+    var trabalhador = trabalhadorRepository.save(trabalhadorMapper.toEntity(novoTrabalhadorDto));
+    trabalhador.setServicos(listaServicos(novoTrabalhadorDto.getServicosIds()));
+
+    return trabalhadorMapper.toDto(trabalhador);
+  }
+
+  private List<Servico> listaServicos(List<Long> servicosIds) {
+    List<Servico> servicos = new ArrayList<>();
+    for (Long id : servicosIds) {
+      servicos.add(servicoRepository.findById(id).get());
+    }
+
+    return servicos;
+  }
 }
