229e67e98803a6ac2e739cdf420705fe85a70342
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-04-13 01:05:20 -0300

    Adiciona construtor Trabalhador com parâmetro DTO

--- src/main/java/br/com/aforca/admin/domain/entity/Trabalhador.java
+++ src/main/java/br/com/aforca/admin/domain/entity/Trabalhador.java
@@ -1,5 +1,6 @@
 package br.com.aforca.admin.domain.entity;
 
+import br.com.aforca.admin.api.model.NovoTrabalhadorDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -48,7 +50,7 @@ public class Trabalhador {
   @Getter
   @Setter
   @Column(name = "data_ultimo_contrato")
-  private LocalDate dataUltimoCadastro;
+  private LocalDate dataUltimoContrato;
 
   @Getter
   @Setter
@@ -67,4 +69,26 @@ public class Trabalhador {
   @ManyToMany
   @JoinTable(name = "trabalhadores_servicos", joinColumns = @JoinColumn(name = "trabalhador_id"), inverseJoinColumns = @JoinColumn(name = "servico_id"))
   private List<Servico> servicos;
+
+  public Trabalhador(NovoTrabalhadorDto novoTrabalhadorDto) {
+    this.nome = novoTrabalhadorDto.getNome();
+    this.telefone = novoTrabalhadorDto.getTelefone();
+    this.endereco = novoTrabalhadorDto.getEndereco();
+    this.email = novoTrabalhadorDto.getEmail();
+    this.nivel = 1;
+    this.dataCadastro = LocalDate.now();
+    this.dataUltimoContrato = null;
+    this.referencias = novoTrabalhadorDto.getReferencias();
+    this.restricoes = novoTrabalhadorDto.getRestricoes();
+    this.observacoes = novoTrabalhadorDto.getObservacoes();
+
+    List<Servico> servicos = new ArrayList<>();
+    for (Long id : novoTrabalhadorDto.getServicosIds()) {
+      var servico = new Servico();
+      servico.setId(id);
+      servicos.add(servico);
+    }
+
+    this.servicos = servicos;
+  }
 }
