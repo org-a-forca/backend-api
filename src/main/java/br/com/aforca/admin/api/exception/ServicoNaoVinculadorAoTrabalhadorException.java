b79c40e8461edaab74762801c5b7cc327f4c3e0d
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-04-24 22:45:59 -0300

    Implementa exceção para serviço não vinculado ao trabalhador a ser contratado

---
+++ src/main/java/br/com/aforca/admin/api/exception/ServicoNaoVinculadorAoTrabalhadorException.java
@@ -0,0 +1,7 @@
+package br.com.aforca.admin.api.exception;
+
+public class ServicoNaoVinculadorAoTrabalhadorException extends RuntimeException {
+  public ServicoNaoVinculadorAoTrabalhadorException(String message) {
+    super(message);
+  }
+}
