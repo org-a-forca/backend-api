4c731c87d13ed3d4d9a4637d2f5b42b65427f1f0
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-05-07 23:53:59 -0300

    Refatora método de busca do repository do Usuario

--- src/main/java/br/com/aforca/admin/domain/repository/UsuarioRepository.java
+++ src/main/java/br/com/aforca/admin/domain/repository/UsuarioRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-  @Query(value = "select * from usuario where email = :email")
+  @Query(value = "select * from usuario where email = :email", nativeQuery = true)
   Optional<Usuario> findByEmail(String email);
 }
