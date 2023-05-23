e802725373b6a69f93ffc6b1396223bc26bc683b
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-05-22 22:34:20 -0300

    Adiciona exceção para email de usuário já registrado

---
+++ src/main/java/br/com/aforca/admin/api/exception/EmailJaRegistradoException.java
@@ -0,0 +1,7 @@
+package br.com.aforca.admin.api.exception;
+
+public class EmailJaRegistradoException extends RuntimeException {
+  public EmailJaRegistradoException(String message) {
+    super(message);
+  }
+}
