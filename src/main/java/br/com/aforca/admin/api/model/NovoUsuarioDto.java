2aca5196b2467a047713540daa78926dfabcdbbe
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-05-22 22:20:34 -0300

    Adiciona DTOs do Usuario

--- src/main/java/br/com/aforca/admin/api/model/NovoUsuarioDto.java
+++ src/main/java/br/com/aforca/admin/api/model/NovoUsuarioDto.java
@@ -1,18 +1,21 @@
-package br.com.aforca.admin.api.payload.request;
+package br.com.aforca.admin.api.model;
 
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
-public class LoginRequest {
+public class NovoUsuarioDto {
   @NotBlank(message = "O email do usuário é obrigatório")
+  @Size(max = 100, message = "O email do trabalhador deve ter no máximo 100 caracteres")
   @Email(message = "O email do usuário deve ser de formato válido")
   private String email;
 
   @NotBlank(message = "A senha do usuário é obrigatório")
+  @Size(min = 6, message = "A senha do usuário deve ter no mínimo 6 caracteres")
   private String senha;
 }
