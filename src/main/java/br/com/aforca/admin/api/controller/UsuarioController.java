5850b317372a9e2e07a55166c96e14cded63d74b
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-05-22 22:54:25 -0300

    Refatora endpoints da Usuario

--- src/main/java/br/com/aforca/admin/api/controller/UsuarioController.java
+++ src/main/java/br/com/aforca/admin/api/controller/UsuarioController.java
@@ -1,23 +1,26 @@
 package br.com.aforca.admin.api.controller;
 
+import br.com.aforca.admin.api.exception.EmailJaRegistradoException;
 import br.com.aforca.admin.api.model.NovoUsuarioDto;
 import br.com.aforca.admin.api.model.UsuarioDto;
 import br.com.aforca.admin.api.payload.response.MessageResponse;
 import br.com.aforca.admin.api.security.jwt.JwtUtils;
 import br.com.aforca.admin.api.security.service.UserDetailsImpl;
-import br.com.aforca.admin.domain.entity.Usuario;
-import br.com.aforca.admin.domain.repository.UsuarioRepository;
+import br.com.aforca.admin.domain.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -27,25 +30,23 @@ public class UsuarioController {
   AuthenticationManager authenticationManager;
 
   @Autowired
-  UsuarioRepository usuarioRepository;
-
-  @Autowired
-  PasswordEncoder encoder;
-
-  @Autowired
   JwtUtils jwtUtils;
 
+  @Autowired
+  UsuarioService usuarioService;
+
   @PostMapping("/criar")
-  public ResponseEntity<?> registerUser(@RequestBody @Valid NovoUsuarioDto novoUsuarioDto) {
-    if (usuarioRepository.findByEmail(novoUsuarioDto.getEmail()).isPresent()) {
-      return ResponseEntity.badRequest().body(new MessageResponse("Erro: Este email já está sendo utilizado!"));
+  public ResponseEntity<?> registerUser(@RequestBody @Valid @NotNull NovoUsuarioDto novoUsuarioDto) {
+    try {
+      usuarioService.registerUser(novoUsuarioDto);
+
+      return new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (EmailJaRegistradoException ex) {
+      Map<String, Object> corpoDaResposta = new HashMap<>();
+      corpoDaResposta.put("email", ex.getMessage());
+
+      return new ResponseEntity<>(corpoDaResposta, HttpStatus.BAD_REQUEST);
     }
-
-    var usuario = new Usuario(novoUsuarioDto.getEmail(), encoder.encode(novoUsuarioDto.getSenha()));
-
-    usuarioRepository.save(usuario);
-
-    return ResponseEntity.ok(new MessageResponse("Usuário registrado com sucesso!"));
   }
 
   @PostMapping("/logar")
@@ -65,7 +66,6 @@ public class UsuarioController {
   @PostMapping("/deslogar")
   public ResponseEntity<?> logoutUser() {
     ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
-    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
-        .body(new MessageResponse("Você foi deslogado!"));
+    return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(new MessageResponse("Você foi deslogado!"));
   }
 }
