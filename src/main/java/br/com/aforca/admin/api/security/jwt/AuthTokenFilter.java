354d232829208f79c18bec28dec7f5f974e8d9fe
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2023-05-22 21:43:27 -0300

    Refatora filtro do token de autenticação

--- src/main/java/br/com/aforca/admin/api/security/jwt/AuthTokenFilter.java
+++ src/main/java/br/com/aforca/admin/api/security/jwt/AuthTokenFilter.java
@@ -28,30 +28,25 @@ public class AuthTokenFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     try {
-      String jwt = parseJwt(request);
+      var jwt = parseJwt(request);
+
       if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-        String username = jwtUtils.getEmailFromJwtToken(jwt);
-
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-        UsernamePasswordAuthenticationToken authentication =
-            new UsernamePasswordAuthenticationToken(userDetails,
-                null,
-                userDetails.getAuthorities());
+        var username = jwtUtils.getEmailFromJwtToken(jwt);
+        var userDetails = userDetailsService.loadUserByUsername(username);
+        var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
         authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
       }
     } catch (Exception e) {
-      logger.error("Cannot set user authentication: {}", e);
+      logger.error("Não é possível definir a autenticação do usuário: {}", e);
     }
 
     filterChain.doFilter(request, response);
   }
 
   private String parseJwt(HttpServletRequest request) {
-    String jwt = jwtUtils.getJwtFromCookies(request);
-    return jwt;
+    return jwtUtils.getJwtFromCookies(request);
   }
 }
